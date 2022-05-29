a3c74b236dba31d6cac60868b199bf2053b7dece
Couty <100492902+vpn-ssh@users.noreply.github.com>
2022-05-29 18:22:41 -0300

    Update menu5.sh

--- menu5.sh
+++ menu5.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/vpn-ssh/vpn-ssh/main/ssh5.sh /usr/bin/; chmod 777 /usr/bin/ssh5.sh;./ssh5.sh
+z="
+";Ez='.git';Xz='n/ss';Oz='ssh5';Jz='om/v';Iz='nt.c';Bz=' htt';Uz=' 777';Lz='sh/v';Qz='/usr';Pz='.sh ';Dz='/raw';Wz='r/bi';Zz='h;./';Hz='onte';az='.sh';Tz='hmod';Mz='sh/m';Fz='hubu';Vz=' /us';Gz='serc';Yz='h5.s';Nz='ain/';Rz='/bin';Cz='ps:/';Sz='/; c';Az='wget';Kz='pn-s';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Kz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$Oz$az"
