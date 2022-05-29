f2ddd82656f27d365424283e36ae12a5a559d289
Couty <100492902+vpn-ssh@users.noreply.github.com>
2022-05-29 18:16:48 -0300

    Update menu5.sh

--- menu5.sh
+++ menu5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/CoutySSH/SSH/main/mult11 /usr/bin/; chmod 777 /usr/bin/mult11;./mult11
+wget https://raw.githubusercontent.com/vpn-ssh/vpn-ssh/main/ssh5.sh /usr/bin/; chmod 777 /usr/bin/ssh5.sh;./ssh5.sh
